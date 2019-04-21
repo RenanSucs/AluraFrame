ec8629c6e6b275d1531b975d61719a0340a0d25d
RenanSucs <47055395+RenanSucs@users.noreply.github.com>
2019-04-21 12:09:56 -0300

    encapsulamento e congelando objeto na criação

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,23 +1,30 @@
 class Negociacao{
     constructor(data, quantidade, valor){//underline permite apenas que os metodos da classe modifiquem o objeto, ninguem de fora consegue
-        this._data = data;
+        this._data = new Date(data.getTime());//cria uma nova data para que esse valor nao seja alterado
         this._quantidade = quantidade;
         this._valor = valor;
+        Object.freeze(this); //congela o objeto para ele nao ser alterado após ser criado, porém ele é razo, não congela todas as propriedades desse objeto
     };
 
-    getVolume(){
+    get volume(){//utilizamos o get para que o método seja chamado em forma de propriedade. Exemplo: n1.volume/n1.quantidade - sempre vão referenciar o método criado aqui
         return this._quantidade * this._valor;
     };
 
-    getData(){
-        return this._data;
+    get data(){
+        return new Date(this._data.getTime());//cria uma nova data para que esse valor nao seja alterado
     };
 
-    getQuantidade(){
+    get quantidade(){
         return this._quantidade;
     };
 
-    getValor(){
+    get valor(){
         return this._valor;
     };
 }
+
+//ES6 trocar VAR por LET para que a variavel fique dentro apenas do escopo que ela foi criada, não pode ser acessada por fora daquele escopo
+// for(let i = 1; i<=100; i++){
+// console.log(i) //A VARIAVEL i EXISTE APENAS DENTRO DESSE BLOCO, POR CAUSA DO LET  
+//}
+
