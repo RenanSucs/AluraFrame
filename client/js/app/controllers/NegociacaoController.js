15741277f4058f4d7b3a2ec24c61e5626688f51d
RenanSucs <renansucsleite@hotmail.com>
2019-04-22 13:24:11 -0300

    criando métodos

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -3,14 +3,19 @@ class NegociacaoController{
     constructor(){
 
         let $ = document.querySelector.bind(document);//bind querySelector faz com que ele mantenha a associação ao "document", pois ele é um método do objeto "document" e só vai funcionar caso ele mantenha essa relação
-        this.inputData = $('#data');
-        this.inputQuantidade = $('#quantidade');
-        this.inputValor = $('#valor')
+        
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
     }
 
     adiciona(event){
         event.preventDefault(event);
-        console.log(this.inputData.value);
+        let negociacao = new Negociacao(//passando paramentros para a função negociacao
+            this._inputData.value,
+            this._inputQuantidade,
+            this._inputValor
+        )
     };
 
 }
