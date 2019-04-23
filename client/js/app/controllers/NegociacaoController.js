341c2f48fb2d837f08134722246dd55015880528
RenanSucs <47055395+RenanSucs@users.noreply.github.com>
2019-04-22 21:52:22 -0300

    ajuste no objeto data

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -11,11 +11,22 @@ class NegociacaoController{
 
     adiciona(event){
         event.preventDefault(event);
-        let negociacao = new Negociacao(//passando paramentros para a função negociacao
-            this._inputData.value,
-            this._inputQuantidade,
-            this._inputValor
-        )
+        let data = new Date(...//cria objeto data '...'(spread operator) faz com que array receba parametros
+            this._inputData.value
+            .split('-')//separa a data em array a cada '-'
+            .map((item, indice)=>{//recebe item = item do array e indice = numero do local do item do array
+                if(indice === 1){//procura a posição 1 do array(dia(0), mes(1),  ano(2))
+                    return item - 1; //decrementa o item do array
+                }
+                return item;
+            })
+        );
+
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
     };
 
 }
